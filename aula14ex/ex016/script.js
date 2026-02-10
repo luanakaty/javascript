b1a5cfda3bca65151eb2b69654292fcf7bf42641
luanakaty <luanakatybs@gmail.com>
2026-02-10 12:02:56 -0300

    Calculadora

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,8 +4,8 @@ function contar() {
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
-        window.alert('[ERRO] faltam dados!')
         res.innerHTML = 'Impossivel contar!'
+        window.alert('[ERRO] faltam dados!')
     } else {
       res.innerHTML ='Contando: <br> '
       let i = Number(ini.value)
@@ -16,12 +16,14 @@ function contar() {
         p = 1
       }
       if (i < f) {
+        // contagem crescente
         for(let c = i; c <= f; c += p) {
            res.innerHTML += ` ${c} \u{1F449}`
        }  
     } else {
+        // contagem regressiva 
         for(let c = i; c >= f; c -= p) {
-            res.innerHTML += `${c} \u{1F449}`
+            res.innerHTML += `${c} \u{1F449} `
         }
     }
     res.innerHTML += `\u{1F3C1}`
